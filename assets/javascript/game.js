3b205d04a76311bc1ee013f36ab036b4e4745fda
sfattal <fattal.sherif@gmail.com>
2018-10-27 16:03:11 -0400

    added timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,25 @@
-https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple
+// Event Listener:
+$("#start-btn").on("click", function() {
+    $("#quiz").empty();
+    $("#timer").text("Time Remaining " + timer);
+    setInterval(decrement, 1000);
+
+});
+
+// Timer:
+var timer = 60;
+
+function decrement() {
+    timer--
+    $("#timer").text("Time Remaining " + timer);
+    if (timer === 0) {
+        stop();
+    }
+}
+
+function stop() {
+// show results screen
+}
+
+// Questions:
+// https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple
