2d8788d9ce9d96d9c888837be78a839d36e7f303
sfattal <fattal.sherif@gmail.com>
2018-10-27 17:29:03 -0400

    finished timer, added results screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,33 @@
 $("#start-btn").on("click", function() {
     $("#quiz").empty();
     $("#timer").text("Time Remaining " + timer);
-    setInterval(decrement, 1000);
+    interval = setInterval(decrement, 1000);
+    if(timer === 0) {
+        stop()
+    }
 
+    //Questions:
+    // var queryURL = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple";
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   })
+    //     .then(function(response) {
+    //         var questions = response.data;
+    //         var questionsDiv = $("<div>");
+    //         console.log(response)
+    //     })
+       
 });
 
-// Timer:
-var timer = 60;
+// Global Variables:
+var timer = 2;
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var interval
 
+// Timer:
 function decrement() {
     timer--
     $("#timer").text("Time Remaining " + timer);
@@ -17,9 +37,16 @@ function decrement() {
     }
 }
 
+// Results Screen:
 function stop() {
-// show results screen
+    var result = $("<h3>Results:</h3>");
+    $("#quiz").append(result);
+    var correctAns = $("<div>Correct: " + correct + "</div>");
+    $("#quiz").append(correctAns);
+    var wrongAns = $("<div>Wrong: " + wrong + "</div>");
+    $("#quiz").append(wrongAns);
+    var unAns = $("<div>Unanswered: " + unanswered + "</div>");
+    $("#quiz").append(unAns);
+    clearInterval(interval);
+    $("#timer").empty();
 }
-
-// Questions:
-// https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple
