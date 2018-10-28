a1faeb5cd68114cfb2a222104f7ee22c6d086f6f
sfattal <fattal.sherif@gmail.com>
2018-10-27 20:15:36 -0400

    added objects from API for questions and answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,22 +7,37 @@ $("#start-btn").on("click", function() {
         stop()
     }
 
-    //Questions:
-    // var queryURL = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple";
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    //   })
-    //     .then(function(response) {
-    //         var questions = response.data;
-    //         var questionsDiv = $("<div>");
-    //         console.log(response)
-    //     })
+    // Questions API:
+    var queryURL = "https://cocktail-trivia-api.herokuapp.com/api/category/animals/difficulty/medium";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+            var questionBank = response;
+            console.log(response)
+            for (var i = 0; i < questionBank.length; i++) {
+                // pulled question and appended to html in a p tag
+                var p = $("<p>" + response[i].text + "<p>");
+                $("#quiz").append(p);
+                // pulled answer choices and appended each to html in a radio button
+                // each group of answers has a different name (line 25) so that only 1 of 4 can be selected i.e. 1 answer
+                var singleSelect = "btn" + i
+                for (j = 0; j < 4; j++) {
+                    var radioBtn = $("<input type='radio'>").val(response[i].answers[j].text);
+                    radioBtn.attr("name", singleSelect);
+                    $("#quiz").append(radioBtn);
+                    var btnText = $("<span>" + response[i].answers[j].text + "</span>")
+                    $("#quiz").append(btnText);
+                }
+            }
+            
+        })
        
 });
 
 // Global Variables:
-var timer = 2;
+var timer = 20;
 var correct = 0;
 var wrong = 0;
 var unanswered = 0;
@@ -39,9 +54,10 @@ function decrement() {
 
 // Results Screen:
 function stop() {
+    $("#quiz").empty();
     var result = $("<h3>Results:</h3>");
     $("#quiz").append(result);
-    var correctAns = $("<div>Correct: " + correct + "</div>");
+    var correctAns = $("<div>Correct: " + correct + "<div>");
     $("#quiz").append(correctAns);
     var wrongAns = $("<div>Wrong: " + wrong + "</div>");
     $("#quiz").append(wrongAns);
